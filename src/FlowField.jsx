e07029f0cfc5883742dcfdd7bc6d852fb49e1189
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2022-09-26 15:54:48 +0200

    p5 sketch

---
+++ src/FlowField.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Sketch from 'react-p5';
+import Particle from './particle';
+
+const FlowField = props => {
+  let inc = 0.1;
+  let scl = 100;
+  let cols, rows;
+
+  let zoff = 0;
+
+  let particles = [];
+
+  let flowfield;
+
+  const setup = (p5, canvasParentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+    p5.createCanvas(500, 500).parent(canvasParentRef);
+    p5.background(255);
+    cols = p5.floor(p5.width / scl);
+    rows = p5.floor(p5.height / scl);
+
+    flowfield = new Array(cols * rows);
+    for (let i = 0; i < 500; i++) {
+      particles[i] = new Particle(p5, cols, rows);
+    }
+  };
+
+  const draw = p5 => {
+    p5.stroke(255);
+    p5.noFill();
+    let yoff = 0;
+    for (let y = 0; y < rows; y++) {
+      let xoff = 0;
+      for (let x = 0; x < cols; x++) {
+        let index = x + y * cols;
+        let angle = p5.noise(xoff, yoff, zoff) * p5.TWO_PI * 4;
+        let v = p5.constructor.Vector.fromAngle(angle);
+        v.setMag(1);
+        flowfield[index] = v;
+        xoff += inc;
+        p5.stroke(0, 50);
+      }
+      yoff += inc;
+      zoff += 0.001;
+    }
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].follow(flowfield);
+      particles[i].update();
+      particles[i].edges();
+      particles[i].show();
+    }
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
+};
+
+export default FlowField;
---
+++ src/particle.js
@@ -0,0 +1,59 @@
+export default function Particle(p5, scl, cols) {
+    this.pos = p5.createVector(p5.random(p5.width), p5.random(p5.height));
+    this.vel = p5.createVector(0, 0);
+    this.acc = p5.createVector(0, 0);
+    this.maxspeed = 20;
+    this.h = 0;
+  
+    this.prevPos = this.pos.copy();
+  
+    this.update = function () {
+      this.vel.add(this.acc);
+      this.vel.limit(this.maxspeed);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+    };
+  
+    this.follow = function (vectors) {
+      let x = p5.floor(this.pos.x / scl);
+      let y = p5.floor(this.pos.y / scl);
+      let index = x + y * cols;
+      let force = vectors[index];
+      this.applyForce(force);
+    };
+  
+    this.applyForce = function (force) {
+      this.acc.add(force);
+    };
+  
+    this.show = function () {
+      p5.stroke(0, 5);
+      p5.strokeWeight(1);
+      p5.line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+      this.updatePrev();
+    };
+  
+    this.updatePrev = function () {
+      this.prevPos.x = this.pos.x;
+      this.prevPos.y = this.pos.y;
+    };
+    this.edges = function () {
+      if (this.pos.x > p5.width) {
+        this.pos.x = 0;
+        this.updatePrev();
+      }
+      if (this.pos.x < 0) {
+        this.pos.x = p5.width;
+        this.updatePrev();
+      }
+      if (this.pos.y > p5.height) {
+        this.pos.y = 0;
+        this.updatePrev();
+      }
+      if (this.pos.y < 0) {
+        this.pos.y = p5.height;
+        this.updatePrev();
+      }
+    };
+  }
+  
